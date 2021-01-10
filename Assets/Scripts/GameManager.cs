fa54a192ed31172fd373fd68ed6e616b7b19e5f0
Admin <Avalyn>
2021-01-09 20:11:02 -0600

    Display Recipe Images on UI
    Mock Recipe added to game manager, displays ingredient items in list on UI under Recipe text label

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+	//ingredient enum
+	const int CHEESE = 0;
+	const int PEPP = 1;
+	const int ONION = 2;
+	const int ANCH = 3;
+	const int MUSH = 4;
+
+	public Image[] ingredImages = new Image[5];
+
+
+
+	public TextMeshProUGUI recipeText;
+
+	static int[] deluxeRecipe = {CHEESE, PEPP, ONION, ANCH, MUSH};
+
+	int[] recipe = deluxeRecipe;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Vector3 ingredListPos = recipeText.transform.position;
+        Vector3 transVector = new Vector3(0, -45, 0);
+        for(int i = 0; i < recipe.Length; i++){
+        	Image ingred = ingredImages[recipe[i]];
+        	ingred.transform.Translate(transVector * i);
+        	ingred.gameObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
