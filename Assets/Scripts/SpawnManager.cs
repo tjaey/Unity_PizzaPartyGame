dee784f35fbb1fe95de4c1b9169e35882182662a
Admin <Avalyn>
2021-01-13 22:21:46 -0600

    Make random spawn rate

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,14 +6,20 @@ public class SpawnManager : MonoBehaviour
 {
 	[SerializeField] GameObject[] ingredPrefabs = new GameObject[5];
 	private float startDelay = 2;
-	private float repeatRate = 2;
+	//private float repeatRate = 2;
 	private float maxX = 7;
 	private float yPos = 17;
 	private float zPos = -2.5f;
+    private float minRate = 1f;
+    private float maxRate = 2.5f;
+
+    GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnIngred", startDelay, repeatRate);
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        StartCoroutine(SpawnIngred());
+        //InvokeRepeating("SpawnIngred", startDelay, repeatRate);
     }
 
     // Update is called once per frame
@@ -22,11 +28,15 @@ public class SpawnManager : MonoBehaviour
         
     }
 
-    void SpawnIngred(){
-    	int ingredNum = Random.Range(0, ingredPrefabs.Length);
-    	GameObject ingred = ingredPrefabs[ingredNum];
-    	float spawnPosX = Random.Range(-maxX, maxX);
-    	Vector3 spawnPos = new Vector3(spawnPosX, yPos, zPos);
-    	Instantiate(ingred, spawnPos, ingred.transform.rotation);
+    IEnumerator SpawnIngred(){
+        while(!gameManager.isGameOver()){
+           float spawnRate = Random.Range(minRate, maxRate);
+    	   yield return new WaitForSeconds(spawnRate);
+           int ingredNum = Random.Range(0, ingredPrefabs.Length);
+    	   GameObject ingred = ingredPrefabs[ingredNum];
+    	   float spawnPosX = Random.Range(-maxX, maxX);
+    	   Vector3 spawnPos = new Vector3(spawnPosX, yPos, zPos);
+    	   Instantiate(ingred, spawnPos, ingred.transform.rotation);
+        }
     }
 }
